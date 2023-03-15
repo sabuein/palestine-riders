d9622a59e72cf4f1b1e54f0d4479133d74ff2455
Salaheddin AbuEin <sabuein@gmail.com>
2023-03-15 02:49:53 +0000

    Removing React and adding Express

---
+++ assets/js/api.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 4435;
+
+app.get("/", (req, res) => {
+    res.send("Bismillah.");
+    console.log(`Listening on http://localhost:${port}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
